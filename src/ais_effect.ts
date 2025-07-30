a44114145d8bec21dd3b2d0350a005f3e08c9835
Frank Behrens <fbehrens@gmail.com>
2025-07-30 10:22:43 +0200

    ais_effect

--- src/ais_effect.ts
+++ src/ais_effect.ts
@@ -1,41 +1,40 @@
 import { Socket } from "@effect/platform";
-import { Effect, Queue, Console } from "effect";
+import { Effect, Queue, Console, Config, Redacted } from "effect";
 import { NodeRuntime } from "@effect/platform-node";
 import WebSocket from "ws";
-import { pipe } from "effect/Function";
-
-const url = "wss://stream.aisstream.io/v0/stream";
-let subscriptionMessage = pipe(
-  {
-    Apikey: "795e0f0b4c7c96849efc40c41e8db3ca57916972",
-    BoundingBoxes: [
-      [
-        [-180, -90],
-        [180, 90],
-      ],
-    ],
-  },
-  JSON.stringify
-);
 
 const main = Effect.gen(function* (_) {
-  const socket = yield* Socket.makeWebSocket(Effect.succeed(url));
+  const socket = yield* Socket.makeWebSocket(
+    Effect.succeed("wss://stream.aisstream.io/v0/stream")
+  );
   const messages = yield* Queue.unbounded<Uint8Array>();
   const fiber = yield* Effect.fork(socket.run((_) => messages.offer(_)));
+  const write = yield* socket.writer;
+  const Apikey = yield* Config.redacted(Config.string("AISSTREAM_API_KEY"));
+  yield* write(
+    JSON.stringify({
+      Apikey: Redacted.value(Apikey),
+      BoundingBoxes: [
+        [
+          [-180, -90],
+          [180, 90],
+        ],
+      ],
+    })
+  );
   yield* Effect.gen(function* () {
-    const write = yield* socket.writer;
-    yield* write(subscriptionMessage);
-  }).pipe(Effect.scoped);
-  let message = yield* messages.take;
-  yield* Console.log(message.toString());
-  return yield* fiber.await;
+    const message = yield* messages.take;
+    yield* Console.log(message.toString());
+  }).pipe(Effect.forever);
+  //   return yield* fiber.await;
 });
 
 const runnable = main.pipe(
   Effect.provideService(
     Socket.WebSocketConstructor,
     (url) => new WebSocket(url)
-  )
+  ),
+  Effect.scoped
 );
 
 NodeRuntime.runMain(runnable);
