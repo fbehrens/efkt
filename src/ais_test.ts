89125c409de442a7132fd09627c0186303893a59
Frank Behrens <fbehrens@gmail.com>
2025-07-31 13:09:06 +0200

    simple ais_test

--- src/ais_test.ts
+++ src/ais_test.ts
@@ -1,5 +1,17 @@
+import { Schema, Effect } from "effect";
 import { assertEquals } from "@std/assert";
 
+const MetaData = Schema.Struct({
+  MMSI: Schema.Number,
+  time_utc: Schema.String,
+});
+const MetaDataValue = Schema.parseJson(MetaData);
+
 Deno.test("schema", () => {
-  assertEquals(1, 1);
+  const json = `{"MMSI": 123456789, "time_utc": "2025-07-30 20:47:10.738288734 +0000 UTC"}`;
+  const md = Schema.decode(MetaDataValue)(json);
+  assertEquals(Effect.runSync(md), {
+    MMSI: 123456789,
+    time_utc: "2025-07-30 20:47:10.738288734 +0000 UTC",
+  });
 });
